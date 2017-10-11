5478d38f9a58eaa9f137adc7a748fed9e2c3a3bc
Bencho Naut <benchonaut__AT__github.com@b1nch0.us.to>
2017-10-12 01:52:03 +0200

    quick_NAT()

--- _y0l0_functions_root.sh
+++ _y0l0_functions_root.sh
@@ -1,2 +1,3 @@
 syncdrop() { echo 3|tee /proc/sys/vm/drop_caches > /dev/null ;sync ; } ;
+quick_NAT() { iptables -I FORWARD  -j ACCEPT -m state --state NEW,RELATED,ESTABLISHED;iptables -I POSTROUTING -t nat -j MASQUERADE;echo "1" > /proc/sys/net/ipv4/ip_forward ; } ;
 

388259e6526e7d7b8f3797f6b412ffbdebef1451
Bencho Naut <benchonaut__AT__github.com@b1nch0.us.to>
2017-10-30 14:12:24 +0100

    2017-10-30_14:12

--- _y0l0_functions_root.sh
+++ _y0l0_functions_root.sh
@@ -1,3 +1,5 @@
 syncdrop() { echo 3|tee /proc/sys/vm/drop_caches > /dev/null ;sync ; } ;
-quick_NAT() { iptables -I FORWARD  -j ACCEPT -m state --state NEW,RELATED,ESTABLISHED;iptables -I POSTROUTING -t nat -j MASQUERADE;echo "1" > /proc/sys/net/ipv4/ip_forward ; } ;
+_quick_NAT() { iptables -I FORWARD  -j ACCEPT -m state --state NEW,RELATED,ESTABLISHED;iptables -I POSTROUTING -t nat -j MASQUERADE;echo "1" > /proc/sys/net/ipv4/ip_forward ; } ;
+_ssh_rsa8192_serverkey() { du -b /etc/ssh/ssh_host_rsa_key|grep ^636[0-9] && echo is 8192 || ssh-keygen -t rsa -b 8192 -f /etc/ssh/ssh_host_rsa_key -N ""; }
+
 

0d3d97620660eb74713f59e796680f719a73a39b
Benjamin Freitag <benjamin.freitag@kosmoskosmos.de>
2017-12-16 22:40:35 +0100

    2017-12-16-21-40

---
+++ fritzrate.php
@@ -0,0 +1,28 @@
+<?php
+// Pollt daten per UPNP aus der Fritzbox
+$client = new SoapClient(
+  null,
+  array(
+    'location'   => "http://192.168.178.1:49000/igdupnp/control/WANCommonIFC1",
+    'uri'        => "urn:schemas-upnp-org:service:WANCommonInterfaceConfig:1",
+    'soapaction' => "",
+    'noroot'     => True
+  )
+);
+
+$status = $client->GetCommonLinkProperties();
+$status2 = $client->GetAddonInfos();
+// print_r($status);
+// print_r($status2);
+// Bytes in Bits umrechnen (Ermittelte Werte * 8)
+$ByteSendRate      = $status2['NewByteSendRate'];
+$ByteSendRateBIT      = $status2['NewByteSendRate']*8;
+$ByteReceiveRate   = $status2['NewByteReceiveRate'];
+$ByteReceiveRateBIT   = $status2['NewByteReceiveRate']*8;
+$NewTotalBytesSent = $status2['NewTotalBytesSent'];
+$NewTotalBytesReceived	= $status2['NewTotalBytesReceived'];
+
+//print_r($status2);
+echo "trafficout:",$ByteSendRate," trafficin:",$ByteReceiveRate," totalout:",$NewTotalBytesSent," totalin:",$NewTotalBytesReceived,"";
+?>
+

7eca7ff37bcfbe9c610d7d120aa000d1849acdad
Benjamin Freitag <benchonaut@yandex.com>
2018-05-18 03:32:25 +0200

    2018-05-18-01-32

--- _y0l0_functions_user.sh
+++ _y0l0_functions_user.sh
@@ -3,6 +3,9 @@ _ssh_pubkey_authcmd() { echo echo $(cat .ssh/id_rsa.pub |cut -d" " -f1-2) "> ~/.
 _sshfs_arcfour_nocomp() { test -d $s || mkdir -p $2 ; sshfs -o Ciphers=arcfour,Compression=no,auto_cache $1 $2 ; } ;
 _last() { last -an400 |sed 's/^\([a-zA-Z0-9]\)[a-zA-Z0-9]\+/\1***/g;s/ \+/ /g;s/\t/ /g;s/mux/µ/g' ; } ;
 _lastn() { last -adn400 |sed 's/^\([a-zA-Z0-9]\)[a-zA-Z0-9]\+/\1***/g;s/ \+/ /g;s/\t/ /g;s/mux/µ/g' ; } ;
-_ssl_valid_in_future_seconds() { openssl x509 -checkend $1 -noout -in "$2" ; } ; #86400 seconds for one day
-_ssl_enddate() { printf '%s: %s\n' "$(date --date="$(openssl x509 -enddate -noout -in "$1"|cut -d= -f 2)" --iso-8601)" "$1"  ; } ; 
- 
+_ssl_pem_valid_in_future_seconds() { openssl x509 -checkend $1 -noout -in "$2" ; } ; #86400 seconds for one day
+_ssl_pem_enddate() { printf '%s: %s\n' "$(date --date="$(openssl x509 -enddate -noout -in "$1"|cut -d= -f 2)" --iso-8601)" "$1"  ; } ; 
+_ssl_host_valid_in_future_seconds() { echo | openssl s_client -connect "$2" 2>/dev/null | openssl x509 -checkend $1 -noout  ; } ; #86400 seconds for one day
+_ssl_host_enddate() { printf '%s: %s\n' "$(date --date="$( echo | openssl s_client -connect "$2" 2>/dev/null |openssl x509 -enddate -noout |cut -d= -f 2)" --iso-8601)" "$1"  ; } ; 
+_ssl_smtp_valid_in_future_seconds() { echo |openssl s_client -connect "$2" -starttls smtp 2>/dev/null | openssl x509 -checkend $1 ; } ; #86400 seconds for one day
+_ssl_smtp_enddate() { printf '%s: %s\n' "$(date --date="$( echo | openssl s_client -connect "$2" -starttls smtp 2>/dev/null | openssl x509 -enddate -noout |cut -d= -f 2)" --iso-8601)" "$1"  ; } ; 
